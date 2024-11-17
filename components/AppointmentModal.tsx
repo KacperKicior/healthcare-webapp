1ef45f43f2dcdbc588349112dd1d136b1928a126
Kacper <kiciorkacper@gmail.com>
2024-11-17 19:47:26 +0100

    Admin Dashboard added with contol actions over appointments

---
+++ components/AppointmentModal.tsx
@@ -0,0 +1,54 @@
+'use client'
+
+import React, {useState} from "react";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger
+} from "@/components/ui/dialog";
+import {Button} from "@/components/ui/button";
+import AppointmentForm from "@/components/forms/AppointmentForm";
+import {Appointment} from "@/types/appwrite.types";
+
+const AppointmentModal = ({
+    type,
+    patientId,
+    userId,
+    appointment,
+}:{
+    type: 'schedule' | 'cancel',
+    patientId: string,
+    userId: string,
+    appointment?: Appointment,
+}) => {
+    const [open, setOpen] = useState(false)
+    return (
+        <Dialog open={open} onOpenChange={setOpen}>
+            <DialogTrigger>
+                <Button variant={"ghost"} className={`capitalize ${type === 'schedule' && 'text-green-500'}`}>{type}</Button>
+            </DialogTrigger>
+            <DialogContent className={"shad-dialog sm:max-w-md"}>
+                <DialogHeader className={"mb-4 space-y-3"}>
+                    <DialogTitle className={"capitalize"}>{type} Appointment</DialogTitle>
+                    <DialogDescription>
+                        Please confirm that you want to {type} this appointment.
+                    </DialogDescription>
+                </DialogHeader>
+
+                <AppointmentForm
+                    userId={userId}
+                    patientId={patientId}
+                    appointment={appointment}
+                    type={type}
+                    setOpen={setOpen}
+                />
+
+            </DialogContent>
+        </Dialog>
+
+    );
+}
+export default AppointmentModal;
--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -1,8 +1,10 @@
 'use server';
 
 import {APPOINTMENT_COLLECTION_ID, DATABASE_ID, databases} from "@/lib/appwrite.config";
-import {ID} from "node-appwrite";
+import {ID, Query} from "node-appwrite";
 import {parseStringify} from "@/lib/utils";
+import {Appointment} from "@/types/appwrite.types";
+import {revalidatePath} from "next/cache";
 
 export const createAppointment = async (appointment: CreateAppointmentParams) => {
     try {
@@ -28,6 +30,68 @@ export const getAppointment = async (appointmentId: string) => {
         )
 
         return parseStringify(appointment);
+    }catch (error) {
+        console.log(error);
+    }
+}
+
+export const getRecentAppoinmentList = async () => {
+    try {
+        const appointments = await databases.listDocuments(
+            DATABASE_ID!,
+            APPOINTMENT_COLLECTION_ID!,
+            [Query.orderDesc('$createdAt')]
+        )
+
+        const initialCounts = {
+            scheduledCount: 0,
+            pendingCount: 0,
+            cancelledCount: 0
+        }
+
+        const counts = (appointments.documents as Appointment[]).reduce(
+            (acc, appointment) => {
+                switch (appointment.status) {
+                    case "scheduled":
+                        acc.scheduledCount++;
+                        break;
+                    case "pending":
+                        acc.pendingCount++;
+                        break;
+                    case "cancelled":
+                        acc.cancelledCount++;
+                        break;
+                }
+                return acc;
+            },
+            initialCounts
+        );
+
+        const data = {
+            totalCount:appointments.total,
+            ...counts,
+            documents: appointments.documents
+        }
+        return parseStringify(data);
+    }catch (error) {
+        console.log(error);
+    }
+}
+
+export const updateAppointment = async ({appointmentId, userId, appointment, type}: UpdateAppointmentParams) => {
+    try {
+        const updatedAppointment = await databases.updateDocument(
+            DATABASE_ID!,
+            APPOINTMENT_COLLECTION_ID!,
+            appointmentId,
+            appointment
+        )
+        if (!updatedAppointment) {
+            throw new Error("Appointment not found");
+        }
+
+        revalidatePath('/admin');
+        return parseStringify(updatedAppointment);
     }catch (error) {
         console.log(error);
     }
