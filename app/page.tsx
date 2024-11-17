13d02de9b89707e81f365340cc59e534e3a782ce
Kacper <kiciorkacper@gmail.com>
2024-11-17 16:54:43 +0100

    Added Alert Dialog with OTP verification for admin panel

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import PatientForm from "@/components/forms/PatientForm";
 import Link from "next/link";
+import PasskeyModal from "@/components/PasskeyModal";
 
-export default function Home() {
+export default function Home({searchParams}:SearchParamProps) {
+    const isAdmin = searchParams.admin === "true";
   return (
     <div className={"flex h-screen max-h-screen"}>
+        {isAdmin && <PasskeyModal />}
         <section className={"remove-scrollbar container my-auto"}>
             <div className={"sub-container max-w-[496px]"}>
                 <Image src="/assets/icons/logo-full.svg" height={1000} width={1000} alt="Patient" className="mb-12 h-10 w-fit"/>
